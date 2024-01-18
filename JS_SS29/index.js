c276a9e1c77d09d564b68006f4a89a557162454d
Hiepduong040 <147359314+Hiepduong040@users.noreply.github.com>
2024-01-18 07:35:00 +0700

    Add files via upload

---
+++ JS_SS29/index.js
@@ -0,0 +1,10 @@
+
+
+function triangleS(){
+    let a = 20;
+    let h = 20;
+    let s =(a * h)/2;
+
+    console.log("Diện tích của tam giác:", s);
+}
+// tại 1 nơi nào đó trong chương trình tương tự
